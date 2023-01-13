a465ff29219ed35fc5a2bfe4e1ce898121c028d4
odelolajosh <oladapoodelola@gmail.com>
2023-01-13 01:17:01 +0100

    task

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -8,8 +8,8 @@ BEGIN
 	DECLARE weight_avg INT DEFAULT 0;
 	
 	SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
-	INTO weight_avg
-	FROM corrections JOIN projects ON corrections.project_id = projects.id
+	INTO weight_avg FROM corrections
+	JOIN projects ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
 	
 	UPDATE users SET users.average_score = IFNULL(weight_avg, 0)
