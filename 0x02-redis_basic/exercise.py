e1eda07e98c7e69e963bcf9f85154f761f1ce24e
odelolajosh <oladapoodelola@gmail.com>
2023-01-18 23:40:50 +0100

    task: reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Cache """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 Data = Union[str, bytes, int, float]
 
@@ -20,3 +20,16 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Data:
+        """ Gets the value from Redis using a key. """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """ Gets a string from Redis using a key. """
+        return get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """ Gets an integer from Redis using a key. """
+        return get(key, int)
