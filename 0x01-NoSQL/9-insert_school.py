9ebd8b6ee9e6d6ae21fbb8a115cf624e07ae4070
odelolajosh <oladapoodelola@gmail.com>
2023-01-18 22:05:09 +0100

    task: misc

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,8 +2,8 @@
 """ 9. Insert a document in Python """
 
 
-def insert_school(mongo_collection, *kwargs):
+def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs. """
-    doc = mongo_collection.insert_one(kwargs)
-    return doc.inserted_id
+    new = mongo_collection.insert_one(kwargs)
+    return new.inserted_id
 
