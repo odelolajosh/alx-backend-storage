a09c45d31f7a15935bdd38e6d9e73e8222e51c55
odelolajosh <oladapoodelola@gmail.com>
2023-01-19 21:58:17 +0100

    fix: track how many times a particular URL was accessed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,16 +13,17 @@ def counter_url(fn: Callable) -> Callable:
         """ wrapper function. """
         if not args:
             return fn(*args, **kwargs)
-        
+
         cache = redis.Redis()
         key = f"result:{args[0]}"
         count_key = f"count:{args[0]}"
 
-        cache.incr(count_key)
         if cache.exists(key):
+            cache.incr(count_key)
             return cache.get(key)
 
         result = fn(*args, **kwargs)
+        cache.incr(count_key)
         cache.setex(key, 10, result)
         return result
     return wrapper
@@ -33,3 +34,13 @@ def get_page(url: str) -> str:
     """ obtain content of a URL. """
     r = requests.get(url)
     return r.text
+
+
+if __name__ == "__main__":
+    url = "https://www.google.com"
+    cache = redis.Redis()
+    cache.flushdb()
+    print(get_page(url))
+    print(get_page(url))
+    count_key = f"count:{url}"
+    print("get_page is called {} times".format(int(cache.get(count_key))))
