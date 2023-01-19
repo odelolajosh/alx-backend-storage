f0c953a0cc4c694d31d02c11281d2de832c6b636
odelolajosh <oladapoodelola@gmail.com>
2023-01-19 22:03:32 +0100

    fix: track how many times a particular URL was accessed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,9 +8,6 @@ from typing import Callable
 
 def get_page(url: str) -> str:
     """ obtain content of a URL. """
-    if not url or type(url) is not str:
-            return ""
-
     cache = redis.Redis()
     key = f"result:{url}"
     count_key = f"count:{url}"
