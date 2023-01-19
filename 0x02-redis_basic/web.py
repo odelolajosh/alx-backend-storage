e0ba622cb4897f722403629698652ce3e43f061e
odelolajosh <oladapoodelola@gmail.com>
2023-01-19 22:02:47 +0100

    fix: track how many times a particular URL was accessed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,34 +6,23 @@ from functools import wraps
 from typing import Callable
 
 
-def counter_url(fn: Callable) -> Callable:
-    """ counter decorator. """
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        """ wrapper function. """
-        if not args:
-            return fn(*args, **kwargs)
-
-        cache = redis.Redis()
-        key = f"result:{args[0]}"
-        count_key = f"count:{args[0]}"
-
-        if cache.exists(key):
-            cache.incr(count_key)
-            return cache.get(key)
-
-        result = fn(*args, **kwargs)
-        cache.incr(count_key)
-        cache.setex(key, 10, result)
-        return result
-    return wrapper
-
-
-@counter_url
 def get_page(url: str) -> str:
     """ obtain content of a URL. """
-    r = requests.get(url)
-    return r.text
+    if not url or type(url) is not str:
+            return ""
+
+    cache = redis.Redis()
+    key = f"result:{url}"
+    count_key = f"count:{url}"
+
+    if cache.exists(key):
+        cache.incr(count_key)
+        return cache.get(key)
+
+    result = requests.get(url).text
+    cache.incr(count_key)
+    cache.setex(key, 10, result)
+    return result
 
 
 if __name__ == "__main__":
