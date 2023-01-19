0e00e2bc00790b42dd8fb3f4689db94a3769f4be
odelolajosh <oladapoodelola@gmail.com>
2023-01-19 21:39:32 +0100

    task: implement web cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,26 +6,26 @@ from functools import wraps
 from typing import Callable
 
 
-def counter(fn: Callable) -> Callable:
+def counter_url(fn: Callable) -> Callable:
     """ counter decorator. """
     @wraps(fn)
-    def wrapper(*args, **kwargs) -> str:
+    def wrapper(url: str) -> str:
         """ wrapper function. """
         cache = redis.Redis()
-        res_key = f"result:{args[0]}"
-        count_key = f"count:{args[0]}"
+        res_key = f"result:{url}"
+        count_key = f"count:{url}"
 
         if cache.exists(res_key):
             cache.incr(count_key)
             return cache.get(res_key)
 
-        result = fn(*args, **kwargs)
+        result = fn(url)
         cache.setex(res_key, 10, result)
         return result
     return wrapper
 
 
-@counter
+@counter_url
 def get_page(url: str) -> str:
     """ obtain content of a URL. """
     r = requests.get(url)
