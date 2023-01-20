e383a54a9fe4b632bff6e1f4b6a726659262f363
odelolajosh <oladapoodelola@gmail.com>
2023-01-20 08:07:30 +0100

    chore: new impl

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,27 +6,27 @@ from functools import wraps
 from typing import Callable
 
 
+def counter_url(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> str:
+        url = args[0]
+        cache = redis.Redis()
+        key = f"result:{url}"
+        count_key = f"count:{url}"
+
+        cached = cache.get(key)
+        if cached is not None:
+            cache.incr(count_key)
+            return cached.decode("utf-8")
+
+        result = func(*args, **kwargs)
+        cache.incr(count_key)
+        cache.setex(key, 10, result)
+        return result
+    return wrapper
+
+
+@counter_url
 def get_page(url: str) -> str:
     """ obtain content of a URL. """
-    cache = redis.Redis()
-    key = f"result:{url}"
-    count_key = f"count:{url}"
-
-    if cache.exists(key):
-        cache.incr(count_key)
-        return cache.get(key)
-
-    result = requests.get(url).text
-    cache.incr(count_key)
-    cache.setex(key, 10, result)
-    return result
-
-
-if __name__ == "__main__":
-    url = "https://www.google.com"
-    cache = redis.Redis()
-    cache.flushdb()
-    print(get_page(url))
-    print(get_page(url))
-    count_key = f"count:{url}"
-    print("get_page is called {} times".format(int(cache.get(count_key))))
+    return requests.get(url).text
