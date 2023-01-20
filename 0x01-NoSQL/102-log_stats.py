b59066d473bc7a58cd66e759c54204752d72d9b6
odelolajosh <oladapoodelola@gmail.com>
2023-01-21 00:50:16 +0100

    chore: use format

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ 12. Log stats """
-import pymongo
+from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient("mongodb://127.0.0.1:27017")
     nginx = client.logs.nginx
 
     count = nginx.count_documents({})
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     print("IPs:")
     ips = nginx.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$count": {}}}},
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ])
