c53bbfa6f578c581b5d57353303481d6758d0eed
odelolajosh <oladapoodelola@gmail.com>
2023-01-21 00:45:05 +0100

    chore: use format

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ 12. Log stats """
-from pymongo import MongoClient
+from pymongo
 
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
     nginx = client.logs.nginx
 
     count = nginx.count_documents({})
